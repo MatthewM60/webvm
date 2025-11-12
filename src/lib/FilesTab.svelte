bfe3327ee08b53000935a7419ed5abe9eafe7a8a
MatthewM60 <murn2343@vandals.uidaho.edu>
2025-11-12 15:42:44 -0800

    b

--- src/lib/FilesTab.svelte
+++ src/lib/FilesTab.svelte
@@ -14,10 +14,71 @@
 	{
 		dispatch('openDownloadModal');
 	}
+
+	function getButtonText(state)
+	{
+		if(state == "START")
+			return "Create File";
+		else if (state == "ADDING")
+			return "Creating...";
+		else if (state == "SUCCESS")
+			return "File Created!";
+		else
+			return "Error Creating File";
+	}
+
+	function getBgColor(state)
+	{
+		if(state == "START")
+		{
+			// Use default
+			return undefined;
+		}
+		else if (state == "SUCCESS")
+		{
+			return "bg-green-900";
+		}
+		else if (state == "ERROR")
+		{
+			return "bg-red-900";
+		}
+		else
+		{
+			return "bg-blue-900";
+		}
+	}
+
+	function getHoverColor(state)
+	{
+		if(state == "START")
+		{
+			// Use default
+			return undefined;
+		}
+		else if (state == "SUCCESS")
+		{
+			return "hover:bg-green-700";
+		}
+		else if (state == "ERROR")
+		{
+			return "hover:bg-red-700";
+		}
+		else
+		{
+			return "hover:bg-blue-700";
+		}
+	}
 </script>
 
 <h1 class="text-lg font-bold">Files</h1>
 <div class="flex flex-col gap-2">
+	<PanelButton 
+		buttonIcon="fa-solid fa-file-plus" 
+		clickHandler={handleAddFile} 
+		buttonText={getButtonText(state)} 
+		bgColor={getBgColor(state)} 
+		hoverColor={getHoverColor(state)}>
+	</PanelButton>
 	<PanelButton 
 		buttonIcon="fa-solid fa-upload" 
 		clickHandler={handleOpenUploadModal} 
@@ -33,7 +94,14 @@
 		hoverColor="hover:bg-purple-700">
 	</PanelButton>
 </div>
-<p><span class="font-bold">Upload Files: </span>Click the upload button to open a file picker and upload files from your computer.</p>
-<p><span class="font-bold">Download Files: </span>Click the download button to retrieve files from /home/user/files/ to your computer.</p>
-<p>Files are stored in the main filesystem and will persist across page reloads.</p>
+{#if message}
+	<p class="{state === 'SUCCESS' ? 'text-green-400' : state === 'ERROR' ? 'text-red-400' : 'text-blue-400'}">
+		<span class="font-bold">{state === 'SUCCESS' ? 'Success: ' : state === 'ERROR' ? 'Error: ' : 'Info: '}</span>{message}
+	</p>
+{:else}
+	<p><span class="font-bold">Create Files: </span>Generate a new file in /home/user/files/ using a terminal command.</p>
+	<p><span class="font-bold">Upload Files: </span>Click the upload button to open a file picker and upload files from your computer.</p>
+	<p><span class="font-bold">Download Files: </span>Click the download button to retrieve files from /home/user/files/ to your computer.</p>
+	<p>Files are stored in the main filesystem and will persist across page reloads.</p>
+{/if}
 
